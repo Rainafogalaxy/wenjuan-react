c2891e50c42379a524ed4c862c89fbd2df0e837e
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-02-05 18:10:27 +0800

    start

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from "react";
+import style from "./QuestionCard.module.scss";
+import { spawn } from "child_process";
+
+type PropsType = {
+  _id: string;
+  title: string;
+  isStar: boolean;
+  isPublished: boolean;
+  answerCount: number;
+  createdAt: string;
+};
+
+const QuestionCard: FC<PropsType> = (props: PropsType) => {
+  const { _id,title,createdAt ,answerCount,isPublished} = props;
+  return (
+    <div className={style.container}>
+      <div className={style.title}>
+        <div className={style.left}>
+            <a href="#">{title}</a>
+        </div>
+        <div className={style.right}>
+            {isPublished?<span style={{color:"green"}}>已发布</span>:<span style={{color:"red"}}>未发布</span>}
+            &nbsp;
+            <span>答卷:{answerCount}</span>
+            &nbsp;
+            <span>{createdAt}</span>
+        </div>
+      </div>
+      <div>button</div>
+    </div>
+  );
+};
+
+export default QuestionCard;
---
+++ src/pages/List.tsx
@@ -0,0 +1,70 @@
+import React, { FC, useState } from "react";
+import QuestionCard from "../components/QuestionCard";
+import style from "./List.module.scss";
+const rawQuestionList = [
+  {
+    _id: "q1",
+    title: "问卷1",
+    isPublished: false,
+    isStar: true,
+    answerCount: 5,
+    createdAt: "1.12 15:20",
+  },
+  {
+    _id: "q2",
+    title: "问卷2",
+    isPublished: true,
+    isStar: false,
+    answerCount: 3,
+    createdAt: "1.12 15:20",
+  },
+  {
+    _id: "q3",
+    title: "问卷3",
+    isPublished: false,
+    isStar: true,
+    answerCount: 4,
+    createdAt: "1.12 15:20",
+  },
+  {
+    _id: "q4",
+    title: "问卷4",
+    isPublished: true,
+    isStar: false,
+    answerCount: 5,
+    createdAt: "1.12 15:20",
+  },
+  {
+    _id: "q5",
+    title: "问卷5",
+    isPublished: false,
+    isStar: true,
+    answerCount: 2,
+    createdAt: "1.12 15:20",
+  },
+];
+const List: FC = () => {
+  const [questionList, setquestionList] = useState(rawQuestionList);
+  return (
+    <>
+      {/* 1 */}
+      <div className={style.header}>
+        <div className={style.left}>
+          <h3>我的问卷</h3>
+        </div>
+        <div className={style.right}>搜索</div>
+      </div>
+      {/* 2 */}
+      <div className={style.content} >
+        {questionList.map((item) => {
+          const { _id } = item;
+          return <QuestionCard key={_id} {...item} />;
+        })}
+      </div>
+      {/* 3 */}
+      <div className={style.footer}>footer</div>
+    </>
+  );
+};
+
+export default List;
