61abc928eba46bd9a5b7b6b4871d3c906e465f0e
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-02-07 18:35:32 +0800

    nn

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,19 +1,56 @@
 import React, { FC } from "react";
-import { Outlet } from "react-router-dom";
-import {Button} from 'antd';
+import { Outlet, useNavigate,useLocation } from "react-router-dom";
+import { Button, Space, Divider } from "antd";
 import style from "./ManageLayout.module.scss";
+import {
+  BarsOutlined,
+  DeleteOutlined,
+  PlusOutlined,
+  StarOutlined,
+} from "@ant-design/icons";
 const ManageLayout: FC = () => {
+  const nav = useNavigate();
+  const {pathname} = useLocation(); //可以判断当前是哪个页面
   return (
     <div className={style.container}>
       <div className={style.left}>
-        <p>ManageLayout Left</p>
-        <Button type="primary" size="large">创建问卷</Button>
-        <br />
-        <a href="#">我的问卷</a>
-        <br />
-        <a href="#">星标问卷</a>
-        <br />
-        <a href="#">回收站</a>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            新建问卷
+          </Button>
+          <Divider style={{ borderTop: "transparent" }} />
+          <Button
+            type={pathname.startsWith('/manage/list')?'default':'text'}
+            size="large"
+            icon={<BarsOutlined />}
+            onClick={() => {
+              nav("/manage/list");
+            }}
+          >
+            我的问卷
+          </Button>
+
+          <Button
+            type={pathname.startsWith('/manage/star')?'default':'text'}
+            size="large"
+            icon={<StarOutlined />}
+            onClick={() => {
+              nav("/manage/star");
+            }}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type={pathname.startsWith('/manage/trash')?'default':'text'}
+            size="large"
+            icon={<DeleteOutlined />}
+            onClick={() => {
+              nav("/manage/trash");
+            }}
+          >
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={style.right}>
         <Outlet />
