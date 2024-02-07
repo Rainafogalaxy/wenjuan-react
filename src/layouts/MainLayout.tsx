8d5efeaa0fce793c1ab7cb5e1db76665e0f22de0
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-02-07 16:41:02 +0800

    none

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import style from './MainLayout.module.scss';
+const { Header, Content, Footer } = Layout;
+
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout Header</div>
-      <div>
-        {/* Outlet类似于Vue的slot */}
-        <Outlet />
-      </div>
-      <div>MainLayout Footer</div>
-    </>
+    <Layout>
+      <Header ></Header>
+      <Content>
+        <Outlet /> {/* Outlet类似于Vue的slot */}
+      </Content>
+      <Footer>MainLayout Footer</Footer>
+    </Layout>
   );
 };
 
