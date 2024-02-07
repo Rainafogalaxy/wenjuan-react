76ab8414db365d59d8dd8c52517f512accd79fb3
Azi <yuwuyinhe@163.com>
2024-02-07 23:18:02 +0800

    not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,28 @@
 import React, { FC } from "react";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+import { MANAGE_INDEX_PATHNAME } from "../router";
 const NotFound: FC = () => {
-  return <p>NotFound</p>;
+  const nav = useNavigate();
+  return (
+    <>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            type="primary"
+            onClick={() => {
+              nav(MANAGE_INDEX_PATHNAME);
+            }}
+          >
+            Back Home
+          </Button>
+        }
+      />
+    </>
+  );
 };
 
 export default NotFound;
