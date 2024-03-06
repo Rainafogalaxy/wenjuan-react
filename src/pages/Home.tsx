99a362339ae814a256ec3d5140049bbb0ca8d4dc
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-03-06 15:39:15 +0800

    12

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { Button, Typography } from "antd";
 import { MANAGE_INDEX_PATHNAME } from "../router";
 import style from "./Home.module.scss";
+
+// test  mockjs
+// import "../_mock/index"; //引用mock文件
+// import axios from "axios";
+
 const { Title, Paragraph } = Typography;
 const Home: FC = () => {
+  // tip:React18 useEffect() 在开发环境下会执行两次 (React 为了帮助发现副作用错误，会故意重复调用生命周期方法)
+  // tip2:mockjs只能劫持XMLRequest，不能劫持fetch请求
+  // axios内部使用的是XML API
+  // useEffect(() => {
+  //   axios.get("/api/test").then((res) => {
+  //     console.log(res);
+  //   });
+  // }, []);
   const nav = useNavigate();
   return (
     <div className={style.container}>
