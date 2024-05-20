5e4e4d93fe6522f78e832dc6c7dcf7d2b91d6218
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-05-20 09:53:09 +0800

    2023520

--- test.js
+++ test.js
@@ -1,24 +1,11 @@
-// 手动解析URL的查询字符串，将其转换为一个对象
-const getQueryParams = (url) => {
-  let queryParams = {};
-  let urlObject = new URL(url); //利用URL类解析URL
-  let params = new URLSearchParams(urlObject.search); //利用URLSearchParams处理查询参数
-  params.forEach((value, key) => {
-    queryParams[key] = value;
-  });
-  return queryParams;
-};
-const url =
-  "https://www.google.com/search?q=%E8%B0%B7%E6%AD%8C%E9%83%B5%E7%AE%B1&oq=&gs_lcrp=EgZjaHJvbWUqCQgBEEUYOxjCAzIJCAAQRRg7GMIDMgkIARBFGDsYwgMyCQgCEEUYOxjCAzIJCAMQRRg7GMIDMgkIBBBFGDsYwgMyCQgFEEUYOxjCAzIJCAYQRRg7GMIDMgkIBxBFGDsYwgPSAQwyMjE1NjQ2ajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8";
-const params = getQueryParams(url);
-console.log(params);
-
-// 直接从当前页面的URL获取查询参数
-const getQueryParams2 = () => {
-  let queryParams = {};
-  let params = new URLSearchParams(window.location.search); //获取当前URL的查询字符串部分
-  params.forEach((value, key) => {
-    queryParams[key] = value;
-  });
-  return queryParams;
-};
+const webSocket = new webSocket("ws://example.com/socket");
+const state = webSocket.readyState;
+if (state === WebSocket.CONNECTING) {
+  //表示连接正在建立阶段
+} else if (state === webSocket.OPEN) {
+  //表示连接已经建立且可以通信
+} else if (state === webSocket.CLOSING) {
+  //表示连接正在关闭的过程
+}else if(state === webSocket.CLOSED){
+  // 表示连接已经关闭或者未能建立连接
+}
