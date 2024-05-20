48c2ecb6b82a696527d2216b56c78be567170e7e
Rainafgalaxy <qihui.yang@myflashcloud.com>
2024-05-20 18:23:27 +0800

    520

--- websocket/server.js
+++ websocket/server.js
@@ -1,1 +1,5 @@
-const Websocket = require('ws');
+const Websocket = require("ws");
+const wss = new Websocket.Server({ port: 8080 });
+wss.on("Connection",function connection(ws){
+  console.log("A new client connected");
+});
